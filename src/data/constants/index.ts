ced0705483960443cdac6510ed41300a256deb69
Gustavo Giovanoni <gugiovanoni1234@gmail.com>
2022-12-16 14:09:57 -0300

    RefineClassicBarbershop

--- src/data/constants/index.ts
+++ src/data/constants/index.ts
@@ -1,25 +1,24 @@
 export const barbershopInfo = {
-  name: "Next Gen Barbershop", 
-  city: 'based in Murfreesboro- Tennessee',
+  name: "Refine Classic Barbershop", 
+  city: 'based in Mount Juliet- Tennessee',
   xp: ``,
 
-  representing: `Bringing to Murfreesboro the most licensed and professional work possible. This is what makes Next Gen Barbershop  a different and a special place. Enjoy our always clean and professional barber shop environment.`,
-  address: '2705 Old Fort Pkwy, Murfreesboro, TN 37128',
+  representing: `Bringing to Mount Juliet the most licensed and professional work possible. This is what makes Refine Classic Barbershop a different and a special place. Enjoy our always clean and professional barber shop environment.`,
+  address: '2231 N. Mount Juliet Rd. Ste 104 Mount Juliet, TN 37122',
   
-  label: 'Next Gen Barbershop',
-  phone:`
-  (615) 907-4006`,
-    miniAgenda: `Mon-Fri	09:00am-5:45pm`,
-    email: `nexgenbarbers@yahoo.com`,
-    facebook: ``,
-    instagram: `https://www.instagram.com/nexgen_barber_shop/`,
-    latitude: 35.84454139734579,
-    longitude: -86.44575633193007,
-  copyright: 'NextGenBarbershop © 2022',
+  label: 'RefineClassicBarbershop',
+  phone:`615-288-3484`,
+   miniAgenda: `Mon-Fri 	09:00am-7:00pm`,
+   email: ` `,
+   facebook: `https://www.facebook.com/Refineclassicbarbershop/`,
+   instagram: `https://www.instagram.com/refinebarbershop/`,
+   latitude: 36.2012185055081,
+   longitude: -86.51835162945245,
+  copyright: 'RefineClassicBarbershop © 2022',
 };
 
 export const hero = 'The most incredible haircuts in town.'
-export const hero2 = `Bringing to Murfreesboro the most licensed and professional work possible.`;
+export const hero2 = `Enjoy our always clean and professional barber shop environment.`;
 
 export const colors = {
   primary: "#e02424",
@@ -44,7 +43,7 @@ export const colors = {
 
 export const logo = {
   nav: {
-    name: "Next Gen Barbershop",
+    name: "Refine Classic Barbershop",
     text: "text-white",
     border: "border-white",
   },
@@ -74,7 +73,7 @@ export const logo = {
     border: "border-white",
   },
   footer: {
-    name: "Next Gen Barbershop",
+    name: "Refine Classic Barbershop",
     text: "text-white font-light",
     border: "border-white",
   },
@@ -83,45 +82,54 @@ export const logo = {
 export const services =  [
   {
       id:1, 
-      name: 'Classic Cut', 
-      price: '40', 
-      description: 'Basic cut with neck shave. Product and Style included.'
+      name: 'Premium Haircut', 
+      price: '33', 
+      description: 'This cut includes a straight razor on the back of the neck, a hot towel, scalp massage, and shampoo with a custom style.'
   },
 
   {
       id:2, 
-      name: 'Detailed Cut', 
-      price: '30', 
-      description: 'Razer fade, hard parts, razor lines, etc. Product and Style included.'
+      name: 'Classic Haircut', 
+      price: '28', 
+      description: 'This cut includes a straight razor on the back of the neck, a hot towel, and a custom style'
   },
 
   {
       id:3, 
-      name: 'Full Beard Trim', 
-      price: '35', 
-      description: 'Kids 10 and younger'
+      name: 'Child’s Cut', 
+      price: '20', 
+      description: 'Kids 12 and under. Each cut includes a hot towel and a custom style.'
   },
 
   {
       id:4, 
-      name: "Edge Up", 
-      price: '35', 
-      description: "All around the head, face not included" 
+      name: "Buzz Cut", 
+      price: '20', 
+      description: "1 guard all over. Each cut includes a straight razor on the back of the neck and a hot towel." 
+  },
+  {
+    id: 5,
+    name: "Skin Fade",//
+    price: "30",//
+    description: "This cut includes a straight razor finish and a hot towel.",
+    // 
   },
 
+
  
-];
+]; 
 
 export const theEstablishment = 
   `The ${barbershopInfo.name} is a place decorated in our own way and always with good music, ${barbershopInfo.representing}. Besides a great haircut, at ${barbershopInfo.name} you can enjoy a place where men can go to be our friends. We want them to feel comfortable here. So come get a haircut and a cold one at ${barbershopInfo.name}!`;
 
+
 export const workingHours = {
-  Monday: `09:00am ⟹ 5:45pm`,
-  Tuesday: "09:00am ⟹ 5:45pm",
-  Wednesday: "09:00am ⟹ 5:45pm",
-  Thursday: "09:00am ⟹ 5:45pm",
-  Friday: "09:00am ⟹ 5:45pm",
-  Saturday: "09:00 ⟹ 2:00pm",
+  Monday: `09:00am ⟹ 7:00pm`,
+  Tuesday: "09:00am ⟹ 7:00pm",
+  Wednesday: "09:00am ⟹ 6:00pm",
+  Thursday: "09:00am ⟹ 7:00pm",
+  Friday: "09:00am ⟹ 7:00pm",
+  Saturday: "08:00am ⟹ 4:00pm",
   Sunday: "Closed",
 };
 
@@ -139,16 +147,15 @@ export const images = [
   "/images/gallery/gallery3.jpg",
   "/images/gallery/gallery4.jpg",
   "/images/gallery/gallery5.jpg",
-  "/images/gallery/gallery6.jpg",
-  "/images/gallery/gallery7.jpg",
+
 ];
 
 export const about = 
-{
+    {
 
-    description: `${barbershopInfo.name} is your Personal Barbershop, based in ${barbershopInfo.city}. We are specialized in all types of men's haircuts and we do our best to please our customers. With the Style you desire by a consultation that fits your lifestyle, ${barbershopInfo.xp} delivering a great result so they feel better when they walk out my door.`,
-    address: barbershopInfo.address,
-}
+        description: `${barbershopInfo.name} is your Personal Barbershop, based in ${barbershopInfo.city}. We are specialized in all types of men's haircuts and we do our best to please our customers. With the Style you desire by a consultation that fits your lifestyle, ${barbershopInfo.xp} delivering a great result so they feel better when they walk out my door.`,
+        address: barbershopInfo.address,
+    }
 ;
 
 
